abdc5484cb330180eba15e89eb6e42ece13db06f
ElChaviSPG <jaestradag@alumno.uspg.edu.gt>
2026-05-30 15:52:51 -0600

    fix(academico): cliente Prisma separado para sistema-academico usando DATABASE_URL

---
+++ webapp/src/lib/prisma-academico.js
@@ -0,0 +1,35 @@
+import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import pg from 'pg';
+
+const globalForPrisma = globalThis;
+
+function createClient() {
+  const url = process.env.DATABASE_URL;
+  if (url) {
+    const pool = new pg.Pool({
+      connectionString: url,
+      ssl: { rejectUnauthorized: false },
+    });
+    const adapter = new PrismaPg(pool);
+    return new PrismaClient({ adapter });
+  }
+  return new PrismaClient();
+}
+
+const prismaAcademico = new Proxy(
+  {},
+  {
+    get(_, prop) {
+      if (!globalForPrisma._prismaAcademicoClient) {
+        globalForPrisma._prismaAcademicoClient = createClient();
+      }
+      const client = globalForPrisma._prismaAcademicoClient;
+      const value = client[prop];
+      return typeof value === 'function' ? value.bind(client) : value;
+    },
+  }
+);
+
+export default prismaAcademico;
+export { prismaAcademico as prisma };
